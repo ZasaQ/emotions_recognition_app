ccb0e33415df386a63ef89f7bf5da0572f9c9ebb
Jan Kubowicz <j.kubowicz7@gmail.com>
2025-06-12 02:35:45 +0200

    WIP main.dart: included authentication handler, routing, removed template components

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:developer' as developer;
 
 import 'package:emotions_recognition_app/firebase_options.dart';
+import 'package:emotions_recognition_app/pages/home_page.dart';
+import 'package:emotions_recognition_app/pages/login_page.dart';
+import 'package:emotions_recognition_app/pages/register_page.dart';
+import 'package:emotions_recognition_app/theme_data.dart';
+import 'package:emotions_recognition_app/utilities.dart';
 
 
 void main() async {
@@ -10,66 +17,89 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Route<dynamic>> onGeneratedInitialRoutes(String initialRouteName) {
+    List<Route<dynamic>> pageStack = [];
+
+    pageStack.add(MaterialPageRoute(builder: (_) => const CheckAuthenticationStatus()));
+
+    return pageStack;
+  }
+
+  Route<dynamic>? onGenerateRoute(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
+    case "/":
+      return MaterialPageRoute(builder: (_) => const CheckAuthenticationStatus());
+    case "/home":
+      return MaterialPageRoute(builder: (_) => const HomePage());
+    case "/login":
+      return MaterialPageRoute(builder: (_) => const LoginPage());
+    case "/register":
+      return MaterialPageRoute(builder: (_) => const RegisterPage());
+    default:
+      return MaterialPageRoute(
+        builder: (_) => const Scaffold(body: Center(child: Text('Route not found'))),
+      );
+  }
+}
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Face Recognition App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Face Recognition App'),
+      navigatorKey: MyApp.navigatorKey,
+      onGenerateInitialRoutes: onGeneratedInitialRoutes,
+      onGenerateRoute: onGenerateRoute,
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      theme: appTheme
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class CheckAuthenticationStatus extends StatefulWidget {
+  const CheckAuthenticationStatus({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CheckAuthenticationStatus> createState() => _CheckAuthenticationStatusState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CheckAuthenticationStatusState extends State<CheckAuthenticationStatus> {
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseAuth.instance.authStateChanges().listen((User? user) async {
+      if (user != null) {
+        MyApp.navigatorKey.currentState!.pushReplacementNamed("/home");
+
+        String? idToken = await user.getIdToken(true);
+        appLog("idToken: $idToken");
+
+        developer.log(
+          name: "CheckAuthenticationStatus -> initState",
+          "Current user is NOT NULL, pushing Home page.");
+      } else {
+        MyApp.navigatorKey.currentState!.pushReplacementNamed("/login");
+
+        developer.log(
+          name: "CheckAuthenticationStatus -> initState",
+          "Current user is NULL, pushing Login page.");
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return const Center(child: CircularProgressIndicator());
   }
 }
